6b364bd100f1213403990dc54e743da4af3f978c
aequanimitas <htavenido@gmail.com>
2024-01-20 14:57:23 +0800

    oops indent

--- 1_1000/lc637/js/index.js
+++ 1_1000/lc637/js/index.js
@@ -16,30 +16,30 @@ function TreeNode(val, left, right) {
  * @return {number}
  */
 var averageOfLevels = function(root) {
-	if (!root) return 0;
+    if (!root) return 0;
 
-	// store root
-	let q = [root], acc = []
+    // store root
+    let q = [root], acc = []
 
-	while(q.length > 0) {
-		let sze = q.length, sum = 0, i = 0
+    while(q.length > 0) {
+        let sze = q.length, sum = 0, i = 0
 
-		while(sze > 0) {
-			sze--
-			i++
-			let node = q.shift()
-			sum += node.val
-			if (node.left != null) q.push(node.left)
-			if (node.right != null) q.push(node.right)
-		}
+        while(sze > 0) {
+            sze--
+            i++
+            let node = q.shift()
+            sum += node.val
+            if (node.left != null) q.push(node.left)
+            if (node.right != null) q.push(node.right)
+        }
 
-		acc.push(sum / i)
-	}
+        acc.push(sum / i)
+    }
 
-	return acc
+    return acc
 };
 
 module.exports = {
-	averageOfLevels: averageOfLevels,
-	TreeNode: TreeNode
+    averageOfLevels: averageOfLevels,
+    TreeNode: TreeNode
 }
